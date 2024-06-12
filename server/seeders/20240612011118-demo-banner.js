6f5721dc80f80ba2b91a19a70edc15723546fe98
Reza Valovi <rezavalovi98@gmail.com>
2024-06-12 09:02:29 +0700

    seed data users,banners, and services to database

---
+++ server/seeders/20240612011118-demo-banner.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const dataBanners = require('../data/banners.json').map((el) => {
+      el.createdAt = el.updatedAt = new Date()
+      return el;
+    })
+    await queryInterface.bulkInsert("Banners", dataBanners, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Banners', null, {});
+  }
+};
